77b53670440e62337552479fd753e47d1edba599
Carter Sifferman <cpsiff@gmail.com>
2022-08-30 15:33:58 -0500

    set end points

--- src/whiteboard.py
+++ src/whiteboard.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+from math import sqrt
 import rospy
 import traceback
 from virtual_whiteboard.msg import Marker
+from std_msgs.msg import String
 import pygame
 
 # get global type parameters from rosparam
@@ -11,13 +13,26 @@ screen_h_px = rospy.get_param('/screen_h_px')
 screen_w_meters = rospy.get_param('/screen_w_meters')
 background = rospy.get_param('/background')
 img_path = rospy.get_param('/img_path')
+endpoint = tuple(rospy.get_param('/endpoints')[background])
+endpoint_radius = rospy.get_param('/endpoint_radius')[background]
 
 METERS_PER_PIXEL = screen_w_meters / screen_w_px
 
-def listener():
+task_status = "not_started"
 
-    # initialize this node
+def main():
+    global task_status
+
     rospy.init_node('whiteboard', anonymous=False)
+
+    # create a publisher for the status of the whiteboard tracing task
+    # will publish "not started", "started", or "finished" when status changes
+    status_pub = rospy.Publisher('/whiteboard_status', String, queue_size=1)
+
+    # give the publisher time to boot up
+    rospy.sleep(3)
+
+    status_pub.publish(String(task_status))
 
     pygame.init()
 
@@ -34,12 +49,20 @@ def listener():
         
         screen.blit(bg, (bg_x, bg_y))
 
+    # for debugging - draw the end point and its radius as a red circle
+    pygame.draw.circle(
+        screen,
+        (255, 0, 0),
+        (endpoint[0], screen_h_px - endpoint[1]),
+        endpoint_radius,
+    )
+
     pygame.display.flip()
 
     # start subscribing to the marker position topic
-    # call whiteboard_draw whenever a new Point object is published on the
-    # /marker_position topic. Point's z coordinate is ignored
-    rospy.Subscriber('/marker_position', Marker, whiteboard_draw, (screen, spray))
+    # call whiteboard_draw whenever a new Marker msg is published on the
+    # /marker_position topic.
+    rospy.Subscriber('/marker_position', Marker, whiteboard_draw, (screen, spray, endpoint, endpoint_radius, status_pub))
 
     # rospy.spin()
     r = rospy.Rate(1)
@@ -50,22 +73,25 @@ def listener():
         r.sleep()
 
 def whiteboard_draw(msg_in, params):
-    screen, spray = params
+    global task_status
+
+    screen, spray, endpoint, endpoint_radius, status_pub = params
     x_coord = msg_in.x / METERS_PER_PIXEL
     y_coord = msg_in.y / METERS_PER_PIXEL
+
     if msg_in.mode.lower() == "draw":
         pygame.draw.circle(
             screen,
-            (0, 0, 0),
+            tuple(rospy.get_param('/draw_color')),
             (x_coord, screen_h_px - y_coord), # flip y axis so origin is bottom left
-            7
+            rospy.get_param('/draw_radius'),
         )
     elif msg_in.mode.lower() == "erase":
         pygame.draw.circle(
             screen,
             (255, 255, 255),
             (x_coord, screen_h_px - y_coord), # flip y axis so origin is bottom left
-            40
+            rospy.get_param('/erase_radius'),
         )
     elif msg_in.mode.lower() == "spray":
         screen.blit(
@@ -75,11 +101,20 @@ def whiteboard_draw(msg_in, params):
     else:
         rospy.logerr(f"Unknown marker mode {msg_in.mode}")
     pygame.display.flip()
-    rospy.loginfo(f"{msg_in.mode} at {x_coord}, {y_coord}")
+    # rospy.loginfo(f"{msg_in.mode} at {x_coord}, {y_coord}")
+
+    # check if the task is done by checking how close it is to the end point
+    if ((sqrt((x_coord-endpoint[0])**2 + (y_coord-endpoint[1])**2) < endpoint_radius) and
+    task_status != "finished"):
+        task_status = "finished"
+        status_pub.publish(String(task_status))
+    elif task_status != "started" and task_status != "finished":
+        task_status = "started"
+        status_pub.publish(String(task_status))
 
 
 if __name__ == '__main__':
     try:
-        listener()
+        main()
     except rospy.ROSInterruptException:
         traceback.print_exc()
