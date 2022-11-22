c3ce3f9c669c2ce0f200a6cb0dc2ca50e9eabd34
yepw <yepingw@outlook.com>
2022-11-22 13:46:42 -0600

    user study

--- src/robot_listener.py
+++ src/robot_listener.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Bool
 from sensor_msgs.msg import JointState
 from virtual_whiteboard.msg import Marker
-from ur5_optimization.msg import EEPoseGoals
 from geometry_msgs.msg import Pose
 from virtual_whiteboard.srv import Clear, ClearResponse
-from omni_msgs.msg import OmniFeedback
+from omni_msgs.msg import OmniButtonEvent
 import transformations as T
 import tf
 import numpy as np
@@ -19,77 +18,97 @@ class RobotListener:
     def __init__(self):
         self.task = "train"
         self.task_state = "start"
+        self.tilted_time = 0
         self.listener = tf.TransformListener()
-        self.marker_position_pub = rospy.Publisher('/marker_position', Marker, queue_size = 1)
-        # self.force_feedback_pub = rospy.Publisher('/phantom/force_feedback', OmniFeedback, queue_size = 1)
-        self.clear_board = rospy.ServiceProxy('clear', Clear)
-        rospy.Timer(rospy.Duration(0.01), self.timer_cb)
+        self.marker_position_pub = rospy.Publisher('whiteboard/draw', Marker, queue_size = 1)
+
+        self.data_log_pub = rospy.Publisher('/data_log', String, queue_size = 1)
+
+        self.clear_board = rospy.ServiceProxy('whiteboard/clear', Clear)
+        rospy.Timer(rospy.Duration(0.005), self.timer_cb)
         rospy.Subscriber('study_state', String, self.study_state_cb)
+        rospy.Subscriber('user_start',  Bool, self.user_start_cb)
+        rospy.Subscriber('/phantom/button', OmniButtonEvent, self.button_cb)
+    
+
+    def user_start_cb(self, msg):
+        pass
 
     def study_state_cb(self, msg):
-        self.task = msg.data.split('_')[1]
-        self.task_state = msg.data.split('_')[2]
-        print(self.task)
-        if self.task_state == 'start':
-            self.clear_board()
+        if len(msg.data.split('_')) >= 2:
+            self.task = msg.data.split('_')[1]
+            if self.task.split('-')[0] == 'practice':
+                self.task = self.task.split('-')[1]
+            self.task_state = msg.data.split('_')[2]
+            if self.task_state == 'ready':
+                self.tilted_time = 0
+            print(self.task)
 
+    def button_cb(self, msg):
+        if self.task_state != 'running':
+            return
+        if msg.grey_button == 1 and self.task == 'spray':
+            marker_position = Marker()
+            marker_position.x = -self.trans[1] + 0.5
+            marker_position.y = self.trans[2] 
+            marker_position.radius = 40
+            marker_position.mode = "spray"
+            self.marker_position_pub.publish(marker_position)
+            
     def timer_cb(self, event):
-
-        if self.task_state != 'start':
+        if self.task_state != 'running':
             return
             
         try:
-            (trans,rot) = self.listener.lookupTransform('/base_link', '/tool_tip', rospy.Time(0))
+            (self.trans, self.rot) = self.listener.lookupTransform('/base_link', '/tool_tip', rospy.Time(0))
         except:
             return
 
-        # feedback = OmniFeedback()
-        # if (trans[0] > 0.8):
-        #     feedback.force.z = (trans[1]-0.8) 
-        # else:
-        #     feedback.force.z = 0.0
-
-        # self.force_feedback_pub.publish(feedback)
-
-        if self.task == "wipe" or self.task == 'write' or self.task == 'spray':
-            marker_position = Marker()
-            # marker tip not on the whiteboard
-            if self.task == "write":
-                if (trans[0] < 0.785):
-                    return
-            elif self.task == "wipe":
-                if (trans[0] < 0.78):
-                    return
-
-            marker_position.x = -trans[1] + 0.5
-            marker_position.y = trans[2] 
-
-            if self.task == "wipe":
-                marker_position.radius = 60
-                marker_position.mode = "erase"
-            elif self.task == "spray":
-                marker_position.radius = 40
-                marker_position.mode = "spray"
-            elif self.task == "write":
-                marker_position.radius = 10
-                marker_position.mode = "draw"
-
-            self.marker_position_pub.publish(marker_position)
-
+        if self.task == 'write' or self.task == 'train':
+            self.handle_write_task()
+        elif self.task == 'spray':
+            pass
+        elif self.task == "wipe":
+            self.handle_wipe_task() 
         elif self.task == "water":
-            curr_ee_rot = [rot[3], rot[0], rot[1], rot[2]]
-            goal_rot_goal = [0.5, 0.5, 0.5, 0.5]
-            scaled_axis = T.quaternion_to_scaledAxis(T.quaternion_dispQ(curr_ee_rot, goal_rot_goal))
-            rx = scaled_axis[0]
-            ry = scaled_axis[1]
-            rz = scaled_axis[2]
-            angle = np.sqrt(rx**2 + rz**2)
-            if angle > 0.17:
-                os.system('play -nq -t alsa synth 0.3 sine 440')
-
+            self.handle_water_task()
         else:
             return
 
+    def handle_write_task(self):
+        # marker tip not on the whiteboard
+        if (self.trans[0] < 0.785):
+            return
+        marker_position = Marker()
+        marker_position.x = -self.trans[1] + 0.5
+        marker_position.y = self.trans[2] 
+        marker_position.radius = 10
+        marker_position.mode = "draw"
+        self.marker_position_pub.publish(marker_position)
+
+    def handle_wipe_task(self):
+        # marker tip not on the whiteboard
+        if (self.trans[0] < 0.78):
+            return
+        marker_position = Marker()
+        marker_position.x = -self.trans[1] + 0.5
+        marker_position.y = self.trans[2] 
+        marker_position.radius = 80
+        marker_position.mode = "erase"
+        self.marker_position_pub.publish(marker_position)
+
+    def handle_water_task(self):
+        curr_ee_rot = [self.rot[3], self.rot[0], self.rot[1], self.rot[2]]
+        goal_rot_goal = [0.5, 0.5, 0.5, 0.5]
+        scaled_axis = T.quaternion_to_scaledAxis(T.quaternion_dispQ(curr_ee_rot, goal_rot_goal))
+        rx = scaled_axis[0]
+        ry = scaled_axis[1]
+        rz = scaled_axis[2]
+        angle = np.sqrt(rx**2 + rz**2)
+        if angle > 0.17:
+            os.system('play -nq -t alsa synth 0.3 sine 440')
+            self.tilted_time += 0.3
+            self.data_log_pub.publish(String("tilted_time:" + str(self.tilted_time)))
 
 if __name__ == '__main__':
     rospy.init_node("whiteboard_listen_to_robot")
--- src/whiteboard.py
+++ src/whiteboard.py
@@ -1,147 +1,201 @@
 #!/usr/bin/env python3
 
 from math import sqrt
+from tkinter import S
+
 import rospy
+import rospkg
 import traceback
 from virtual_whiteboard.msg import Marker
 from virtual_whiteboard.srv import Clear, ClearResponse
 from std_msgs.msg import String
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, Int64
 import pygame
+import numpy as np
+import random
+from subprocess import call
 
 # get global type parameters from rosparam
 screen_w_px = rospy.get_param('/screen_w_px')
 screen_h_px = rospy.get_param('/screen_h_px')
 screen_w_meters = rospy.get_param('/screen_w_meters')
-background = rospy.get_param('/background')
-img_path = rospy.get_param('/img_path')
-endpoint = tuple(rospy.get_param('/endpoints')[background])
-endpoint_radius = rospy.get_param('/endpoint_radius')[background]
+img_path = rospkg.RosPack().get_path('virtual_whiteboard') + "/img"
 
 METERS_PER_PIXEL = screen_w_meters / screen_w_px
 
-task_status = "not_started"
+coverage_threshold = 1000
 
-def main():
-    global task_status
+class Whiteboard:
+    def __init__(self):
+        # set system volumn
+        call(["amixer", "-D", "pulse", "sset", "Master", "35%"])
 
-    rospy.init_node('whiteboard', anonymous=False)
+        global task_status
 
-    # create a publisher for the status of the whiteboard tracing task
-    # will publish "not started", "started", or "finished" when status changes
-    status_pub = rospy.Publisher('/whiteboard_status', String, queue_size=1)
+        # create a publisher for the status of the whiteboard tracing task
+        # will publish "not started", "started", or "finished" when status changes
+        self.user_finish_pub = rospy.Publisher('/user_finish', Bool, queue_size = 1)
+        self.data_log_pub = rospy.Publisher('/data_log', String, queue_size = 1)
 
-    # give the publisher time to boot up
-    rospy.sleep(3)
+        self.pen_points = ""
 
-    status_pub.publish(String(task_status))
+        # give the publisher time to boot up
+        rospy.sleep(1)
 
-    pygame.init()
+        pygame.init()
 
-    screen = pygame.display.set_mode((screen_w_px, screen_h_px), pygame.NOFRAME, display=rospy.get_param('/display_num'))
-    screen.fill((255, 255, 255))
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
 
-    spray = pygame.image.load(f"{img_path}/spray.png").convert_alpha()
+        self.screen = pygame.display.set_mode((screen_w_px, screen_h_px), pygame.NOFRAME, display=rospy.get_param('/display_num'))
+        self.screen.fill((255, 255, 255))
 
-    draw_background(screen, background)
+        self.spray = pygame.image.load(f"{img_path}/spray.png").convert_alpha()
 
-    # for debugging - draw the end point and its radius as a red circle
-    pygame.draw.circle(
-        screen,
-        (255, 0, 0),
-        (endpoint[0], screen_h_px - endpoint[1]),
-        endpoint_radius,
-    )
-
-    pygame.display.flip()
-
-    # start subscribing to the marker position topic
-    # call whiteboard_draw whenever a new Marker msg is published on the
-    # /marker_position topic.
-    rospy.Subscriber('/marker_position', Marker, whiteboard_draw, (screen, spray, endpoint, endpoint_radius, status_pub))
-    
-    # create a service that clears the board
-    # the lambda function is a workaround to pass extra parameters to the handler
-    # https://answers.ros.org/question/247540/pass-parameters-to-a-service-handler-in-rospy/
-    rospy.Service("clear", Clear, lambda msg: handle_clear_srv(msg, (screen, background)))
-
-    rospy.Timer(rospy.Duration(0.01), timer_cb) 
-    rospy.spin()
-    # r = rospy.Rate(1)
-    # while not rospy.is_shutdown():
-    #     # if we don't call pygame.event.pump() ocassionally, pygame thinks that 
-    #     # the program has frozen
-    #     pygame.event.pump()
-    #     r.sleep()
-
-
-def timer_cb(event):
-    pygame.event.pump()
-    pygame.display.flip()
-
-
-def whiteboard_draw(msg_in, params):
-    global task_status
-
-    screen, spray, endpoint, endpoint_radius, status_pub = params
-    x_coord = msg_in.x / METERS_PER_PIXEL
-    y_coord = msg_in.y / METERS_PER_PIXEL
-
-    if msg_in.mode.lower() == "draw":
-        pygame.draw.circle(
-            screen,
-            tuple(rospy.get_param('/draw_color')),
-            (x_coord, screen_h_px - y_coord), # flip y axis so origin is bottom left
-            msg_in.radius
-        )
-    elif msg_in.mode.lower() == "erase":
-        pygame.draw.circle(
-            screen,
-            # (255, 255, 255),
-            (210, 210, 210),
-            (x_coord, screen_h_px - y_coord), # flip y axis so origin is bottom left
-            msg_in.radius
-        )
-    elif msg_in.mode.lower() == "spray":
-        spray = pygame.transform.scale(spray, (msg_in.radius*2, msg_in.radius*2))
-        screen.blit(
-            spray,
-            (x_coord - msg_in.radius, screen_h_px - y_coord - msg_in.radius)
-        )
-    else:
-        rospy.logerr(f"Unknown marker mode {msg_in.mode}")
-    # rospy.loginfo(f"{msg_in.mode} at {x_coord}, {y_coord}")
-
-    # check if the task is done by checking how close it is to the end point
-    if ((sqrt((x_coord-endpoint[0])**2 + (y_coord-endpoint[1])**2) < endpoint_radius) and
-    task_status != "finished"):
-        task_status = "finished"
-        status_pub.publish(String(task_status))
-    elif task_status != "started" and task_status != "finished":
-        task_status = "started"
-        status_pub.publish(String(task_status))
-
-def handle_clear_srv(msg, params):
-    global task_status
-    screen, background = params
-    print("cleared")
-    screen.fill((255, 255, 255))
-    draw_background(screen, background)
-    task_status = "not started"
-    pygame.display.flip()
-    return ClearResponse(True)
-
-def draw_background(screen, background):
-    if background != "":
-        bg = pygame.image.load(f"{img_path}/{background}")
-
-        bg_x = (screen_w_px - bg.get_width())/2
-        bg_y = (screen_h_px - bg.get_height())/2
+        background = rospy.get_param('/background')
+        self.draw_background(background)
         
-        screen.blit(bg, (bg_x, bg_y))
+        pygame.display.flip()
+
+        # start subscribing to the marker position topic
+        # call whiteboard_draw whenever a new Marker msg is published on the
+        # /marker_position topic.
+        rospy.Subscriber('whiteboard/draw', Marker, self.whiteboard_draw)
+        rospy.Subscriber('whiteboard/background', String, self.change_background)
+
+        # create a service that clears the board
+        # the lambda function is a workaround to pass extra parameters to the handler
+        # https://answers.ros.org/question/247540/pass-parameters-to-a-service-handler-in-rospy/
+        rospy.Service("whiteboard/clear", Clear, lambda msg: self.handle_clear_srv)
+
+        rospy.Subscriber('whiteboard/countdown', String, self.update_countdown)
+
+        self.counter = 0
+
+        rospy.Timer(rospy.Duration(0.01), self.timer_cb) 
+
+    def timer_cb(self, event):
+        pygame.event.pump()
+        pygame.display.flip()
+
+    def update_countdown(self, msg):
+        # text = self.font.render("  Time remaining  {} s   ".format(msg.data), True, (0,0,0), (255,255,255))
+        text = self.font.render(msg.data, True, (0,0,0), (255,255,255))
+        textRect = text.get_rect()
+        textRect.center = (screen_w_px // 2, screen_h_px // 15)
+        self.screen.blit(text, textRect)
+
+    def whiteboard_draw(self, msg_in):
+        global task_status
+
+        x_coord = msg_in.x / METERS_PER_PIXEL
+        y_coord = msg_in.y / METERS_PER_PIXEL
+
+        if msg_in.mode.lower() == "draw":
+            pygame.draw.circle(
+                self.screen,
+                tuple(rospy.get_param('/draw_color')),
+                (x_coord, screen_h_px - y_coord), # flip y axis so origin is bottom left
+                msg_in.radius
+            )
+            s = "[" + str(x_coord) + "," + str(screen_h_px - y_coord) + "]"
+            self.data_log_pub.publish(String("pen_points:" + s))
+        elif msg_in.mode.lower() == "erase":
+            pygame.draw.circle(
+                self.screen,
+                (255, 255, 255),
+                (x_coord, screen_h_px - y_coord), # flip y axis so origin is bottom left
+                msg_in.radius
+            )
+            s = "[" + str(x_coord) + "," + str(screen_h_px - y_coord) + "]"
+            # self.data_log_pub.publish(String("pen_points:" + s))
+            # self.counter += 1
+            # if self.counter >= 10:
+            self.data_log_pub.publish(String("unwiped_area:{}".format(self.get_non_white_area())))
+            #     self.counter = 0
+        elif msg_in.mode.lower() == "spray":
+            spray = pygame.transform.scale(self.spray, (msg_in.radius*2, msg_in.radius*2))
+            for i in range(10):
+                self.screen.blit(
+                    spray,
+                    (x_coord - msg_in.radius + random.uniform(-10, 10), screen_h_px - y_coord - msg_in.radius + random.uniform(-10, 10))
+                )
+        else:
+            rospy.logerr(f"Unknown marker mode {msg_in.mode}")
+        # rospy.loginfo(f"{msg_in.mode} at {x_coord}, {y_coord}")
+
+        # check if the task is done
+        return
+
+    def change_background(self, msg):
+        if len(msg.data.split(':')) == 2:
+            file_name = msg.data.split(":")[0]
+            ratio = float(msg.data.split(":")[1])
+            self.draw_background(file_name, ratio)
+        else:
+            self.draw_background(msg.data)
+
+    def handle_clear_srv(self, msg):
+        global task_status
+        print("cleared")
+        self.screen.fill((255, 255, 255))
+        self.draw_background(self.background)
+        task_status = "not started"
+        pygame.display.flip()
+        return ClearResponse(True)
+
+    def draw_background(self, background, ratio = 1.0):
+        self.background = background
+        bg = pygame.image.load(f"{img_path}/{background}")
+        scaled_bg = pygame.transform.scale(bg, (int(bg.get_width()*ratio), int(bg.get_height()*ratio)))
+        bg_x = (screen_w_px - bg.get_width() * ratio) / 2
+        bg_y = (screen_h_px - bg.get_height() * ratio) / 2
+        
+        self.screen.fill((255, 255, 255))
+        self.screen.blit(scaled_bg, (bg_x, bg_y))
+
+        # if background == "ros_curve.svg" or background == "hri_curve.svg" \
+        #     or background == "wipe.svg":
+        #     pygame.display.flip()
+        #     pa = pygame.PixelArray(self.screen)
+        #     non_white_index = np.where(np.array(pa) != int("FFFFFF", 16) )
+        #     self.data_log_pub.publish(
+        #             String("background:{}".format(
+        #                 str(non_white_index[0].tolist()) + 
+        #                 "," + str(non_white_index[1].tolist()))))
+
+        # if background == "hri_curve.svg" or background == "ros_curve.svg" or background == "lab_curve.svg":
+        #     self.terminate_condition = "distance"
+        #     self.endpoint = tuple(rospy.get_param('/endpoints')[background])
+        #     self.endpoint_radius = rospy.get_param('/endpoint_radius')[background]
+
+            # for debugging - draw the end point and its radius as a red circle
+            # pygame.draw.circle(
+            #     self.screen,
+            #     (255, 0, 0),
+            #     (self.endpoint[0], screen_h_px - self.endpoint[1]),
+            #     self.endpoint_radius,
+            # )
+        # if background == "wipe.svg":
+        #     self.terminate_condition = "cover"
+        # elif background == "spray.svg":
+        #     self.terminate_condition = "in_four_regions"
+        #     self.sprayed_regions = [False, False, False, False]
+        # else:
+        #     self.terminate_condition = None
+        if background == "wipe.svg":
+            self.data_log_pub.publish(String("unwiped_area:{}".format(self.get_non_white_area())))
+
+    def get_non_white_area(self):
+        # get the number of pixels that are not white
+        pa = pygame.PixelArray(self.screen)
+        a = np.array(pa[:, 100:])
+        non_white_counts = np.count_nonzero(a != int("FFFFFF", 16) )
+        pa.close()
+        # print("non_white_counts", non_white_counts)
+        return non_white_counts
 
 if __name__ == '__main__':
-    try:
-        main()
-    except rospy.ROSInterruptException:
-        traceback.print_exc()
+
+    rospy.init_node('whiteboard', anonymous=False)
+    wb = Whiteboard()
+    rospy.spin()
